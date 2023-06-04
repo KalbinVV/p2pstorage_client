7839fd04ffcecee437fff2d691582f2b27970a44
Kalbin Vladimir <kalbin.wow@yandex.ru>
2023-06-04 19:31:05 +0500

    Add connection lost package handling

--- PackagesHandlers.py
+++ PackagesHandlers.py
@@ -1,9 +1,18 @@
-from p2pstorage_core.server.Package import Package
+import logging
+
+from p2pstorage_core.server.Package import Package, PackageType, ConnectionLostPackage
 
 from StorageClient import StorageClient
 
 
 def handle_package(package: Package, storage_client: StorageClient) -> None:
-    pass
+    match package.get_type():
+        case PackageType.CONNECTION_LOST:
+            handle_connection_lost(package, storage_client)
 
+
+def handle_connection_lost(package: Package, storage_client: StorageClient) -> None:
+    logging.info(f'Lost connection from server {storage_client.get_server_address()}: {package.get_data()}')
+
+    storage_client.set_running(False)
 
