e751d564e18662f98ac9246f79ea71aa393f17ef
Kalbin Vladimir <kalbin.wow@yandex.ru>
2023-06-04 05:18:30 +0500

    Add correct disconnection mechanism

--- StorageClient.py
+++ StorageClient.py
@@ -1,32 +1,38 @@
 import logging
 import socket
+import threading
 
 from p2pstorage_core.helper_classes.SocketAddress import SocketAddress
+from p2pstorage_core.server.Exceptions import EmptyHeaderException
 from p2pstorage_core.server.Package import PackageType, ConnectionRequestPackage, Package, ConnectionResponsePackage
 
 
 class StorageClient:
-    def __init__(self, server_address: SocketAddress):
+    def __init__(self):
         self.__client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-        self.__server_address = server_address
 
         self.__address = None
 
-    def run(self):
+        self.__running = False
+
+        self.__server_address = '', 0
+
+    def is_running(self) -> bool:
+        return self.__running
+
+    def set_running(self, running: bool) -> None:
+        self.__running = running
+
+    def run(self, server_address: SocketAddress):
+        self.__server_address = server_address
+
         logging.info(f'Try to connect to {self.__server_address}...')
 
         if self.try_connect():
             logging.info(f'Successful connected to {self.__server_address}!')
 
-            running = True
-
-            while running:
-                try:
-                    pass
-                except KeyboardInterrupt:
-                    running = False
-
+            self.set_running(True)
+            self.handle_connection()
         else:
             logging.warning(f'Can\'t connect to {self.__server_address}!')
 
@@ -50,3 +56,21 @@ class StorageClient:
 
         except ConnectionError:
             return False
+
+    def handle_connection(self):
+        while self.is_running():
+            try:
+                package = Package.recv(self.__client_socket)
+            except EmptyHeaderException:
+                logging.warning('Lost connection from server!')
+
+                self.set_running(False)
+                break
+
+            logging.debug(f'Package from server: {package}')
+
+            if package.get_type() == PackageType.CONNECTION_LOST:
+                reason = package.get_data()
+
+                logging.info(f'Connection from server lost: {reason}')
+                self.set_running(False)
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import logging
+import threading
+from typing import Optional
 
 from p2pstorage_core.helper_classes.SocketAddress import SocketAddress
 
@@ -8,16 +10,35 @@ from StorageClient import StorageClient
 def main():
     logging.basicConfig(level=logging.DEBUG)
 
-    server_address = SocketAddress('localhost', 5000)
+    storage_client = StorageClient()
 
-    storage_client = StorageClient(server_address)
+    server_address = input('Enter server address (host:port): ')
 
-    logging.info('Starting...')
+    splitted_server_address = server_address.split(':')
+    host, port = splitted_server_address[0], int(splitted_server_address[1])
 
-    storage_client.run()
+    user_input_thread: Optional[threading.Thread] = None
+
+    try:
+        user_input_thread = threading.Thread(target=user_input_handler, args=(storage_client,))
+        user_input_thread.start()
+
+        storage_client.run(SocketAddress(host, port))
+    except KeyboardInterrupt:
+        pass
 
     logging.info('Stopping...')
 
 
+def user_input_handler(storage_client: StorageClient) -> None:
+    while storage_client.is_running():
+        print('!')
+        user_input = input()
+        print(user_input)
+
+
 if __name__ == '__main__':
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        pass
