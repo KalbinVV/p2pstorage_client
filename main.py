db153336a57f78d8572c2708abcbfc62cbac55a0
Kalbin Vladimir <kalbin.wow@yandex.ru>
2023-06-06 04:28:07 +0500

    Code refactoring, improve logging

--- main.py
+++ main.py
@@ -4,14 +4,25 @@ from CommandsHandlers import init_commands, handle_command
 from StorageClient import StorageClient
 
 
-def main():
+def init_logger() -> None:
     logging.basicConfig(level=logging.DEBUG, filename='logs.log')
 
     # define a Handler which writes INFO messages or higher to the sys.stderr
     console = logging.StreamHandler()
+
     console.setLevel(logging.INFO)
+
+    formatter = logging.Formatter(fmt='[%(asctime)s] [%(levelname)s] > %(message)s',
+                                  datefmt='%Y-%m-%d %H:%M:%S')
+
+    console.setFormatter(formatter)
+
     # add the handler to the root logger
     logging.getLogger('').addHandler(console)
+
+
+def main():
+    init_logger()
 
     storage_client = StorageClient()
 
